74afe1994a4503a46347e857b719ce7af5b6f009
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-03-23 14:05:52 +0100

    Apply feature-based folder structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
+import Layout from '../components/primitives/layout';
+import Image from '../components/primitives/image';
+import SEO from '../components/primitives/seo';
 
 const IndexPage = () => (
     <Layout>
