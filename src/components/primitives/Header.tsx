1445d6c0691c76a12e6bf883856ff1596e82b77f
Martin Broos <martin.broos@freshheads.com>
2020-03-25 17:52:54 +0100

    add maatcss as initial setup for styling

--- src/components/primitives/Header.tsx
+++ src/components/primitives/Header.tsx
@@ -8,29 +8,10 @@ type Props = {
 };
 
 const Header = ({ siteTitle }: Props) => (
-    <header
-        style={{
-            background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
-        }}
-    >
-        <div
-            style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `1.45rem 1.0875rem`,
-            }}
-        >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to={homePath}
-                    style={{
-                        color: `white`,
-                        textDecoration: `none`,
-                    }}
-                >
-                    {siteTitle}
-                </Link>
+    <header className="header">
+        <div className="container">
+            <h1>
+                <Link to={homePath}>{siteTitle}</Link>
             </h1>
         </div>
     </header>
--- src/components/primitives/Layout.tsx
+++ src/components/primitives/Layout.tsx
@@ -9,7 +9,6 @@ import React, { ReactNode } from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 import Header from './Header';
-import './Layout.css';
 
 type Props = {
     children: ReactNode;
@@ -29,13 +28,7 @@ const Layout = ({ children }: Props) => {
     return (
         <>
             <Header siteTitle={data.site.siteMetadata.title} />
-            <div
-                style={{
-                    margin: '0 auto',
-                    maxWidth: 960,
-                    padding: '0 1.0875rem 1.45rem',
-                }}
-            >
+            <div className="container">
                 <main>{children}</main>
             </div>
         </>
