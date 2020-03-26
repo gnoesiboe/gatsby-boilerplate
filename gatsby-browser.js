7d2d9d91035e6c24d5e9ae5542fec12565d15d48
Martin Broos <martin.broos@freshheads.com>
2020-03-26 16:12:25 +0100

    move to using scoped css modules for each component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import './src/styles/app.scss';
+import './src/styles/global.scss';
--- src/components/primitives/Layout.tsx
+++ src/components/primitives/Layout.tsx
@@ -8,7 +8,7 @@
 import React, { ReactNode } from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
-import Header from './Header';
+import Header from './Header/Header';
 
 type Props = {
     children: ReactNode;
